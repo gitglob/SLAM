fb4379ffa634f3557419ad97e8dec13c4593579d
gitglob <pgrigor22@gmail.com>
2023-10-19 22:17:20 +0200

    Add visualization functions

--- visualization/plot_filter_results.py
+++ visualization/plot_filter_results.py
@@ -1,12 +1,11 @@
 # Standard
 import os
 # External
-import numpy as np
 import matplotlib.pyplot as plt
 # Local
 
 
-def plot_filter_trajectories(robot_states, robot_times, ground_truth_states, ground_truth_times, filter_name):
+def plot_filter_trajectories(robot_states, prediction_states, correction_states, ground_truth_states, filter_name):
     """
     Plots and saves the trajectories of the robot and its ground truth.
 
@@ -36,6 +35,9 @@ def plot_filter_trajectories(robot_states, robot_times, ground_truth_states, gro
     # Plot ground truth trajectory
     ax.plot(ground_truth_states[:, 0], ground_truth_states[:, 1], 'r--', label='Ground Truth', alpha=0.7)
     
+    # Plot the correction steps
+    ax.scatter(correction_states[:, 0], correction_states[:, 1], c='g', s=25, alpha=0.5, label='Corrections')
+
     ax.set_title('Robot and Ground Truth Trajectories')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -50,5 +52,5 @@ def plot_filter_trajectories(robot_states, robot_times, ground_truth_states, gro
     save_path = os.path.join("results")
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    plt.savefig(os.path.join(save_path, f"{filter_name}-trajectory_comparison.png"))
+    plt.savefig(os.path.join(save_path, f"{filter_name}-trajectory_comparison.png"), dpi=600)
     plt.close()
