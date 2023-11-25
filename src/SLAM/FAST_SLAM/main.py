19e36bf24982bbf286a4e2b04f0f16559c56bd32
gitglob <pgrigor22@gmail.com>
2023-11-25 14:24:45 +0100

    Add time measurement for prediction and corrections steps

--- src/SLAM/FAST_SLAM/main.py
+++ src/SLAM/FAST_SLAM/main.py
@@ -3,10 +3,9 @@ import os
 import time
 # External
 import numpy as np
-import matplotlib.pyplot as plt
 # Local
 from src.utils import read_world, read_data
-from .utils import Landmark, Particle, resample
+from .utils import Particle, resample
 from .visualization import plot_state
 from .prediction import predict
 from .correction import correct
@@ -34,14 +33,21 @@ def main():
     show_gui = False
 
     # Main loop
+    predict_times = []
+    correct_times = []
     for t in range(data['timestep'].shape[1]):
-        print(f'timestep = {t}')
+        if t%100 == 0:
+            print(f"Time: {t}")
 
         # Prediction step
+        start_time = time.time()
         particles = predict(particles, data['timestep'][t]['odometry'], noise)
+        predict_times.append(time.time() - start_time)
 
         # Correction step
+        start_time = time.time()
         particles = correct(particles, data['timestep'][t]['sensor'])
+        correct_times.append(time.time() - start_time)
 
         # Visualization
         plot_state(particles, landmarks, t, data['timestep'][t]['sensor'], show_gui)
@@ -49,6 +55,17 @@ def main():
         # Resample
         particles = resample(particles)
 
+    print("FAST-SLAM finished!")
+
+    print(f"Avg. prediction time: {sum(predict_times)/len(predict_times):.4f} sec.")
+    print(f"Avg. correction time: {sum(correct_times)/len(correct_times):.4f} sec.")
+    print(f"Avg. step time: {(sum(predict_times) + sum(correct_times))/(len(predict_times) + len(correct_times)):.4f} sec.")
+
+    # Determine the best particle based on the highest weight
+    best_particle_idx = np.argmax([p.weight for p in particles])
+    best_particle = particles[best_particle_idx]
+    print(f"Current state vector: \n{best_particle.pose}")
+    # print(f"Current state covariance: {state_cov}")
 
 if __name__ == "__main__":
     main()
